4c86b033331e61ea06da02637531361b6f3aba60
Elsy Carolina Zambrano <108857263+ElsyCaro@users.noreply.github.com>
2022-09-29 13:03:08 -0500

    boton google

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,6 +11,7 @@ export const Register = () => {
   const inputEmail = document.createElement('input');
   const inputPass = document.createElement('input');
   const inputConfirm = document.createElement('input');
+  const imageGoogle = document.createElement('img');
   div.classList.add('container-Create');
 
   button.textContent = 'Registrar';
@@ -33,6 +34,8 @@ export const Register = () => {
   inputEmail.placeholder = 'Email';
   inputPass.placeholder = 'Contraseña';
   inputConfirm.placeholder = 'Confirmar contraseña';
+  imageGoogle.setAttribute('src', 'https://i.postimg.cc/tJh7mK8T/google.png'); // Se agrego la imagen en vez del boton//
+  imageGoogle.classList = ('buttongoogler');
 
   button.addEventListener('click', () => {
     onNavigate('/');
@@ -40,11 +43,12 @@ export const Register = () => {
   buttonBack.addEventListener('click', () => {
     onNavigate('/');
   });
-  buttonVincule.addEventListener('click', () => {
-    onNavigate('/');
+
+  imageGoogle.addEventListener('click', () => {
+
   });
 
-  div.append(title, inputEmail, inputPass, inputConfirm, button, buttonBack, descriptionRegister, buttonVincule);
+  div.append(title, inputEmail, inputPass, inputConfirm, button, buttonBack, descriptionRegister, imageGoogle);
 
   return div;
 };
